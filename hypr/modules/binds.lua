2e3e8ef1aec3664c6489c24cb667471ef86ea82d
Ryan Chen <153143370+Discr3te@users.noreply.github.com>
2026-09-07 13:19:55 -0500

    s

--- hypr/modules/binds.lua
+++ hypr/modules/binds.lua
@@ -38,64 +38,41 @@ hl.bind(MainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(MainMod .. " + D", hl.dsp.exec_cmd(menu))
 hl.bind(MainMod .. " + Q", hl.dsp.window.close())
 
--- Resize windows with MainMod + Shift + hjkl
-hl.bind(MainMod .. " + SHIFT + R", hl.dsp.submap("resize"))
+-- Resize windows
+hl.bind(MainMod .. " + SHIFT + R", function()
+	hl.dispatch(hl.dsp.submap("resize"))
+	WindowResizeActiveNotification = hl.notification.create({
+		text = " WINDOW RESIZE ACTIVE\n" .. " Precise Resizeing(1px): hjkl\n" .. " Rough Resizeing(5px): SHIFT + hjkl",
+		timeout = 9999999999999999999,
+		icon = "info",
+	})
+end)
+
 hl.define_submap("resize", function()
+	-- precise resizeing(1px)
 	hl.bind("H", hl.dsp.window.resize({ x = -1, y = 0, relative = true }), { repeating = true })
 	hl.bind("L", hl.dsp.window.resize({ x = 1, y = 0, relative = true }), { repeating = true })
 	hl.bind("K", hl.dsp.window.resize({ x = 0, y = 1, relative = true }), { repeating = true })
 	hl.bind("J", hl.dsp.window.resize({ x = 0, y = -1, relative = true }), { repeating = true })
-	hl.bind("escape", hl.dsp.submap("reset"))
+
+	-- rough resizeing(5px)
+	hl.bind("SHIFT + H", hl.dsp.window.resize({ x = -5, y = 0, relative = true }), { repeating = true })
+	hl.bind("SHIFT + L", hl.dsp.window.resize({ x = 5, y = 0, relative = true }), { repeating = true })
+	hl.bind("SHIFT + K", hl.dsp.window.resize({ x = 0, y = 5, relative = true }), { repeating = true })
+	hl.bind("SHIFT + J", hl.dsp.window.resize({ x = 0, y = -5, relative = true }), { repeating = true })
+
+	hl.bind("escape", function()
+		hl.dispatch(hl.dsp.submap("reset"))
+		WindowResizeActiveNotification:set_timeout(1)
+		hl.notification.create({
+			text = " Window Resize Inactive",
+			timeout = 2000,
+			icon = "info",
+		})
+	end)
 end)
 
--- Move focus with MainMod + arrow keys
-hl.bind(MainMod .. " + H", hl.dsp.focus({ direction = "left" }))
-hl.bind(MainMod .. " + L", hl.dsp.focus({ direction = "right" }))
-hl.bind(MainMod .. " + K", hl.dsp.focus({ direction = "up" }))
-hl.bind(MainMod .. " + J", hl.dsp.focus({ direction = "down" }))
-
--- Example special workspace (scratchpad)
-hl.bind(MainMod .. " + S", hl.dsp.workspace.toggle_special("spotify"))
-hl.bind(MainMod .. " + R", hl.dsp.workspace.toggle_special("btop"))
-
--- Scroll through existing workspaces with MainMod + scroll
--- hl.bind(MainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
--- hl.bind(MainMod .. " + mouse_up", hl.dsp.focus({ workspace = "e-1" }))
-
--- Move/resize windows with MainMod + LMB/RMB and dragging
-hl.bind(MainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
-hl.bind(MainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
-
--- Laptop multimedia keys for volume and LCD brightness
-hl.bind(
-	"XF86AudioRaiseVolume",
-	hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
-	{ locked = true, repeating = true }
-)
-hl.bind(
-	"XF86AudioLowerVolume",
-	hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
-	{ locked = true, repeating = true }
-)
-hl.bind(
-	"XF86AudioMute",
-	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
-	{ locked = true, repeating = true }
-)
-hl.bind(
-	"XF86AudioMicMute",
-	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
-	{ locked = true, repeating = true }
-)
-hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
-
--- Requires playerctl
-hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
-hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
-
+-- Workspace
 hl.bind(MainMod .. " + " .. 1, hl.dsp.focus({ workspace = 1 }))
 hl.bind(MainMod .. " + SHIFT + " .. 1, hl.dsp.window.move({ workspace = 1 }))
 
@@ -110,3 +87,57 @@ hl.bind(MainMod .. " + SHIFT + " .. 4, hl.dsp.window.move({ workspace = 4 }))
 
 hl.bind(MainMod .. " + " .. 5, hl.dsp.focus({ workspace = 5 }))
 hl.bind(MainMod .. " + SHIFT + " .. 5, hl.dsp.window.move({ workspace = 5 }))
+
+-- Move focus to window with MainMod + arrow keys
+hl.bind(MainMod .. " + H", hl.dsp.focus({ direction = "left" }))
+hl.bind(MainMod .. " + L", hl.dsp.focus({ direction = "right" }))
+hl.bind(MainMod .. " + K", hl.dsp.focus({ direction = "up" }))
+hl.bind(MainMod .. " + J", hl.dsp.focus({ direction = "down" }))
+
+-- Special workspace (scratchpad)
+hl.bind(MainMod .. " + S", hl.dsp.workspace.toggle_special("spotify"))
+hl.bind(MainMod .. " + R", hl.dsp.workspace.toggle_special("btop"))
+
+-- Move/resize windows with MainMod + LMB/RMB and dragging
+hl.bind(MainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
+hl.bind(MainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
+
+-- Laptop multimedia keys for volume and LCD brightness
+if HOSTNAME == LAPTOP then
+	hl.bind(
+		"XF86AudioRaiseVolume",
+		hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
+		{ locked = true, repeating = true }
+	)
+	hl.bind(
+		"XF86AudioLowerVolume",
+		hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
+		{ locked = true, repeating = true }
+	)
+	hl.bind(
+		"XF86AudioMute",
+		hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
+		{ locked = true, repeating = true }
+	)
+	hl.bind(
+		"XF86AudioMicMute",
+		hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
+		{ locked = true, repeating = true }
+	)
+	hl.bind(
+		"XF86MonBrightnessUp",
+		hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),
+		{ locked = true, repeating = true }
+	)
+	hl.bind(
+		"XF86MonBrightnessDown",
+		hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),
+		{ locked = true, repeating = true }
+	)
+
+	-- Requires playerctl
+	hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
+	hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+	hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+	hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
+end
